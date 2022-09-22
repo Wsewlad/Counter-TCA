a566066a60e9d7be92cbc019b9c7217c8cf15238
vfil <wladyslawfil@gmail.com>
2022-09-22 23:46:02 +0300

    Init project

---
+++ Counter-TCA/Counter-TCA/Counter_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Counter_TCAApp.swift
+//  Counter-TCA
+//
+//  Created by  Vladyslav Fil on 22.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct Counter_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
